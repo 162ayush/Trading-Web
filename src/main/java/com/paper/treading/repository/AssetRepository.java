99acf1649b0e6f2428bed7e9bad5ad6066c9d78f
Ayush <ayushshivhare162@gmail.com>
2025-01-12 22:33:45 +0530

    Added a security filter chain with stateless session management and JWT token validation - Configured CORS to allow frontend access from localhost with specified origins and headers - Disabled CSRF protection as part of stateless authentication
    This update secures API endpoints with JWT authentication and enables CORS for client access.

---
+++ src/main/java/com/paper/treading/repository/AssetRepository.java
@@ -0,0 +1,15 @@
+package com.paper.treading.repository;
+
+import com.paper.treading.modal.Asset;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AssetRepository extends JpaRepository<Asset,Long> {
+
+    List<Asset>findByUserId(Long userId);
+
+    Asset findByUserIdAndCoinId(Long userId, String coinId);
+
+
+}
