fa0a2e5b57c642bc1cc5e1dbe6fe0433f326b561
Ayush <ayushshivhare162@gmail.com>
2025-01-25 23:16:01 +0530

    Added a security filter chain with stateless session management and JWT token validation - Configured CORS to allow frontend access from localhost with specified origins and headers - Disabled CSRF protection as part of stateless authentication
    This update secures API endpoints with JWT authentication and enables CORS for client access.

---
+++ src/main/java/com/paper/treading/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.paper.treading.config;
+
+public class JwtConstant {
+    public static final String SECREATE_KEY="ayusyausyuihshbhdgfyuefrbhdscjbyuefrghusyausy";
+
+    public static final String JWT_HEADER="Authorization";
+}
